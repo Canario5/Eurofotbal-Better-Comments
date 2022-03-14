556c381f1036868f798083b2766954e66e7eae71
Canario5 <48844307+Canario5@users.noreply.github.com>
2022-03-15 00:37:35 +0100

    Ifs replaced by dictionary

--- jump-script.js
+++ jump-script.js
@@ -61,6 +61,40 @@ const posChange = (jumpSize) => {
 	if (jumpSize > 0 && currentPosition < commentsTotal - 1) return miniScroll((currentPosition += jumpSize))
 }
 
+const letterE = () => {
+	//* E letter "previous unread" comment
+	if (currentPosition === "") {
+		if (unreadComments.length) {
+			return miniScroll(lastUnread)
+		}
+		return miniScroll(0, true)
+	}
+	if (currentPosition > unreadComments[0] && currentPosition < lastUnread) {
+		return jumpScroll(false)
+	}
+	if (currentPosition > lastUnread) {
+		return miniScroll(lastUnread)
+	}
+	if (currentPosition === lastUnread && unreadComments.length >= 2) {
+		return miniScroll(unreadComments[unreadComments.length - 2])
+	}
+	blinkingBg()
+}
+
+const letterD = () => {
+	//* D letter "next unread" comment
+	if (currentPosition === "") {
+		if (unreadComments.length) {
+			return miniScroll(unreadComments[0])
+		}
+		return miniScroll(0, true)
+	}
+	if (currentPosition < lastUnread && unreadComments.length) {
+		return jumpScroll(true)
+	}
+	blinkingBg()
+}
+
 const elementScroll = (selectorDummy) => {
 	const elementTarget = document.querySelector(selectorDummy)
 	elementTarget.scrollIntoView({ behavior: "smooth" })
@@ -74,64 +108,24 @@ const blinkingBg = () => {
 	allComments[currentPosition].classList.add("BlinkBlink")
 }
 
+const dict = new Map()
+dict.set("KeyQ", () => posChange(-1)) //* Q letter "UP +1" comment
+dict.set("KeyA", () => posChange(1)) //* A letter "DOWN +1" comment
+dict.set("KeyW", () => posChange(-5)) //* W letter "UP +5" comments
+dict.set("KeyS", () => posChange(5)) //* S letter "DOWN +5" comments
+dict.set("KeyE", () => letterE()) //* E letter "previous unread" comment
+dict.set("KeyD", () => letterD()) //* D letter "next unread" comment
+dict.set("KeyR", () => elementScroll(".article")) //* R letter scroll at the start of the article
+dict.set("KeyF", () => elementScroll(".content.newpost")) //* F letter to the comment box
+
 document.addEventListener("keyup", (e) => {
-	if (!commentsTotal) {
-		return
-	}
-	if (e.code === "KeyQ") {
-		//* Q letter "UP +1" comment
-		return posChange(-1)
-	}
-	if (e.code === "KeyA") {
-		//* A letter "DOWN +1" comment
-		return posChange(1)
-	}
-	if (e.code === "KeyW") {
-		//* W letter "UP +5" comments
-		return posChange(-5)
-	}
-	if (e.code === "KeyS") {
-		//* S letter "DOWN +5" comments
-		return posChange(5)
-	}
-	if (e.code === "KeyE") {
-		//* E letter "previous unread" comment
-		if (currentPosition === "") {
-			if (unreadComments.length) {
-				return miniScroll(lastUnread)
-			}
-			return miniScroll(0, true)
+	if (!commentsTotal) return
+
+	for (const [key, value] of dict.entries()) {
+		if (e.code === key) {
+			return value()
+
+			/* dict.get("KeyS")() */
 		}
-		if (currentPosition > unreadComments[0] && currentPosition < lastUnread) {
-			return jumpScroll(false)
-		}
-		if (currentPosition > lastUnread) {
-			return miniScroll(lastUnread)
-		}
-		if (currentPosition === lastUnread && unreadComments.length >= 2) {
-			return miniScroll(unreadComments[unreadComments.length - 2])
-		}
-		return blinkingBg()
-	}
-	if (e.code === "KeyD") {
-		//* D letter "next unread" comment
-		if (currentPosition === "") {
-			if (unreadComments.length) {
-				return miniScroll(unreadComments[0])
-			}
-			return miniScroll(0, true)
-		}
-		if (currentPosition < lastUnread && unreadComments.length) {
-			return jumpScroll(true)
-		}
-		return blinkingBg()
-	}
-	if (e.code === "KeyR") {
-		//* R letter scroll at the start of the article
-		return elementScroll(".article")
-	}
-	if (e.code === "KeyF") {
-		//* F letter to the comment box
-		return elementScroll(".content.newpost")
 	}
 })
