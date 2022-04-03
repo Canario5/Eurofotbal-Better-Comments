568e3deaa19ab4254f38fb415ec320c1968cade3
Canario5 <48844307+Canario5@users.noreply.github.com>
2022-04-03 22:39:31 +0200

    unreadJumps() splitted

--- scripts/mainScript.js
+++ scripts/mainScript.js
@@ -1,24 +1,22 @@
 import { initComments } from "/scripts/initComments.js"
 import { posChange } from "/scripts/posChange.js"
-import { unreadJump } from "/scripts/unreadJumps.js"
+import { previousUnread, nextUnread } from "/scripts/unreadJumps.js"
 import { parentJump, backToChild } from "/scripts/parentChildJumps.js"
 import { currentPosition } from "/scripts/currentPosition.js"
 
 const mainScript = () => {
 	let currentPos
 	const { commentsTotal } = initComments()
-	const nextUnread = true
-	const previousUnread = false
 
 	const dict = new Map([
 		["KeyQ", () => posChange(-1, currentPos)], //* "UP +1" comment
 		["KeyA", () => posChange(1, currentPos)], //* "DOWN +1" comment
 		["KeyW", () => posChange(-5, currentPos)], //* "UP +5" comments
 		["KeyS", () => posChange(5, currentPos)], //* "DOWN +5" comments
-		["KeyE", () => unreadJump(previousUnread, currentPos)], //* "previous unread" comment
-		["KeyD", () => unreadJump(nextUnread, currentPos)], //* "next unread" comment
+		["KeyE", () => previousUnread(currentPos)], //* "previous unread" comment
+		["KeyD", () => nextUnread(currentPos)], //* "next unread" comment
 		["KeyR", () => parentJump(currentPos)], //* Jump to the parent comment
-		["KeyF", () => backToChild(currentPos)], //* Jump back to original child comment
+		["KeyF", () => backToChild(currentPos)], //* Jump back to original child comment from parent
 		["Digit2", () => elementScroll(".article")], //* Digit2 above letters (not from num. part of keyboard); scroll at the article
 		["KeyX", () => elementScroll(".content.newpost")], //* to the comment box at the bottom */
 		/* ["KeyP", () => console.log("test")], // test button */
--- scripts/unreadJumps.js
+++ scripts/unreadJumps.js
@@ -2,44 +2,49 @@ import { initComments } from "/scripts/initComments.js"
 import { miniScroll } from "/scripts/miniScroll.js"
 import { blinkingBg } from "/scripts/blinkingBg.js"
 
-export { unreadJump }
+export { previousUnread, nextUnread }
 
-const unreadJump = (jumpDirection, currentPos) => {
+const previousUnread = (currentPos) => {
 	const { unreadPositions, lastUnreadPos } = initComments()
 
-	switch (true) {
-		case currentPos === "" && unreadPositions && unreadPositions.length > 0:
-			return miniScroll(unreadPositions[0])
+	if (currentPos === "" && startUnreadJump(unreadPositions) === true) return
 
-		case currentPos === "":
-			miniScroll(0)
-			return blinkingBg()
+	if (currentPos > unreadPositions[0] && currentPos <= lastUnreadPos) {
+		for (let i = currentPos - 1; i >= 0; i--) {
+			if (unreadPositions.indexOf(i) !== -1) return miniScroll(i)
+		}
+		return blinkingBg()
+	}
 
-		case !jumpDirection && currentPos > unreadPositions[0] && currentPos < lastUnreadPos:
-			return jumpScroll(jumpDirection, currentPos, unreadPositions)
-
-		case !jumpDirection && currentPos > lastUnreadPos:
-			return miniScroll(lastUnreadPos)
-
-		case !jumpDirection && currentPos === lastUnreadPos && unreadPositions.length >= 2:
-			return miniScroll(unreadPositions[unreadPositions.length - 2])
-
-		case jumpDirection && currentPos < lastUnreadPos && unreadPositions.length > 0:
-			return jumpScroll(jumpDirection, currentPos, unreadPositions)
+	if (currentPos > lastUnreadPos) {
+		return miniScroll(lastUnreadPos)
 	}
 
 	blinkingBg()
 }
 
-const jumpScroll = (jumpDirection, currentPos, unreadPositions) => {
-	for (let i = 0; i < unreadPositions.length; i++) {
-		if (!(unreadPositions[i] > currentPos)) continue
+const nextUnread = (currentPos) => {
+	const { unreadPositions, lastUnreadPos } = initComments()
 
-		if (jumpDirection === true) return miniScroll(unreadPositions[i])
+	if (currentPos === "" && startUnreadJump(unreadPositions) === true) return
 
-		if (jumpDirection === false && currentPos === unreadPositions[i - 1])
-			return miniScroll(unreadPositions[i - 2])
-
-		if (jumpDirection === false) return miniScroll(unreadPositions[i - 1])
+	if (currentPos < lastUnreadPos && unreadPositions.length > 0) {
+		for (let i = currentPos + 1; i <= lastUnreadPos; i++) {
+			if (unreadPositions.indexOf(i) !== -1) return miniScroll(i)
+		}
+		return blinkingBg()
 	}
+
+	blinkingBg()
+}
+
+const startUnreadJump = (unreadPositions) => {
+	if (!unreadPositions) {
+		miniScroll(0)
+		blinkingBg()
+		return true
+	}
+
+	miniScroll(unreadPositions[0])
+	return true
 }
