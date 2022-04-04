c030476937a00d74ed749e3bb1b5fc0ba061bdbf
Canario5 <48844307+Canario5@users.noreply.github.com>
2022-04-04 14:43:35 +0200

    Changed from If to Switch

--- scripts/posChange.js
+++ scripts/posChange.js
@@ -6,24 +6,25 @@ export { posChange }
 
 const posChange = (jumpSize, currentPos) => {
 	const { commentsTotal } = initComments()
-	if (currentPos === "") return miniScroll(0)
 
-	// Negative jumpSize; Previous, climbing to the top -> comments
-	if (jumpSize < 0 && currentPos < Math.abs(jumpSize)) {
-		miniScroll(0)
-		return blinkingBg()
+	switch (true) {
+		case currentPos === "":
+			return miniScroll(0)
+
+		// Negative jumpSize; Previous, climbing to the top -> comments
+		case jumpSize < 0 && currentPos < Math.abs(jumpSize):
+			miniScroll(0)
+			return blinkingBg()
+
+		case jumpSize < 0 && currentPos > 0:
+			return miniScroll(currentPos + jumpSize)
+
+		// Positive jumpSize; Next, sliding to the bottom -> comments
+		case jumpSize > 0 && currentPos > commentsTotal - (1 + jumpSize):
+			miniScroll(commentsTotal - 1)
+			return blinkingBg()
+
+		case jumpSize > 0 && currentPos < commentsTotal - 1:
+			return miniScroll(currentPos + jumpSize)
 	}
-
-	// prettier-ignore
-	if (jumpSize < 0 && currentPos > 0) 
-        return miniScroll(currentPos + jumpSize)
-
-	// Positive jumpSize; Next, sliding to the bottom -> comments
-	if (jumpSize > 0 && currentPos > commentsTotal - (1 + jumpSize)) {
-		miniScroll(commentsTotal - 1)
-		return blinkingBg()
-	}
-
-	if (jumpSize > 0 && currentPos < commentsTotal - 1)
-		return miniScroll(currentPos + jumpSize)
 }
